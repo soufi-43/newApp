ea9d18480272edd06e23a21745e803873ac6e29c
belhamra soufyane <soufi-43@hotmail.fr>
2019-06-18 00:10:50 +0100

    add sliver to singlePost

---
+++ lib/screens/single_post.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:news_app/models/post.dart';
+
+class SinglePost extends StatefulWidget {
+  final Post post;
+
+  SinglePost(this.post);
+
+  @override
+  _SinglePostState createState() => _SinglePostState();
+}
+
+class _SinglePostState extends State<SinglePost> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: 300.0,
+          floating: true,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: NetworkImage(widget.post.featuredImage),
+                fit: BoxFit.cover,
+              )),
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, position) {
+
+            if(position == 0 ){
+              return Container(
+                height: 500,
+                color: Colors.teal,
+              );
+            }else {
+              return Container(
+                height: 250,
+                color: getRandomColor(),
+              );
+
+            }
+          }, childCount: 25),
+        )
+      ],
+    );
+  }
+
+  Color getRandomColor({int mindBrightness = 50}) {
+    final random = Random();
+    assert(mindBrightness >= 0 && mindBrightness <= 255);
+    return Color.fromARGB(
+      0xFF,
+      mindBrightness + random.nextInt(255 - mindBrightness),
+      mindBrightness + random.nextInt(255 - mindBrightness),
+      mindBrightness + random.nextInt(255 - mindBrightness),
+    );
+  }
+}
